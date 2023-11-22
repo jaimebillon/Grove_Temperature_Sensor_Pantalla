b08e9f9f0fb92e2fe1904f713c9108e719b8cffd
Jaime <jaime.billon@opendeusto.es>
2023-11-22 09:18:56 +0100

    solucion

--- main.cpp
+++ main.cpp
@@ -3,22 +3,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "mbed.h"
 #include "Grove_LCD_RGB_Backlight.h"
+#include "mbed.h"
 
-#define WAIT_TIME_MS 500 
-DigitalOut led1(LED1);
-Grove_LCD_RGB_Backlight rgbLCD(PB_9,PB_8);
 
-int main()
-{
-    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+#define WAIT_TIME_MS 2000
+Grove_LCD_RGB_Backlight rgbLCD(PB_9, PB_8);
 
-    while (true)
-    {
-        rgbLCD.setRGB(0xff, 0xff, 0xff);                 //set the color 
-        rgbLCD.print("Hello World!");
-        led1 = !led1;
-        thread_sleep_for(WAIT_TIME_MS);
-    }
+AnalogIn sensor(A0);
+
+float NTC;
+float temperatura;
+char mensaje[16];
+
+int main() {
+
+  while (true) {
+    NTC = (5.0 / (sensor.read() * 3.3 / 100000.0)) - 100000.0;
+    temperatura = (4250.0 / (log(NTC / 100000.0) + (4250.0 / 298.15))) - 273.15;
+    rgbLCD.locate(0, 0);
+    rgbLCD.setRGB(0xff, 0xff, 0xff); // set the color
+    sprintf(mensaje, "Temp es %.2f", temperatura);
+    rgbLCD.print(mensaje);
+
+    thread_sleep_for(WAIT_TIME_MS);
+  }
 }
